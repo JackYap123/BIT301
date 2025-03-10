31e273f4c488aa4a3113e79c4d3a8f59c5bb51dd
JackYap123 <B2100346@helplive.edu.my>
2025-03-10 11:06:47 +0800

    feat: initial commit

---
+++ inc/sidebar.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="../css/Sidebar.css">
+<div class="sidebar">
+    <h2>SuperConcert</h2>
+    <ul>
+        <li><a href="../php/Dashboard.php">Dashboard</a></li>
+        <li><a href="../php/eventCreation.php">Create Event</a></li>
+        <li><a href="#">Create Ticket</a></li>
+        <li><a href="#">Waiting List</a></li>
+        <li><a href="../php/Profile.php">Create Promotion</a></li>
+    </ul>
+</div>
